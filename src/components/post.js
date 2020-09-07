21de7dcc51a41e7c4dd113da1d7a9dd8a17442d2
Danylo <sukhiy37078@gmail.com>
2020-09-07 03:29:26 +0300

    hw

--- src/components/post.js
+++ src/components/post.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import comment from './images/comment.svg'
+import like from './images/like.svg'
+import repost from './images/repost.svg'
+import share from './images/share.svg'
+import verified from './images/verified.svg'
 
 const Post = (props) =>{
 	return(
@@ -9,7 +14,12 @@ const Post = (props) =>{
 					  <img className="avatar" src={props.author.photo} alt="author" />
 					</div>
 					<div className="profile__info">
-					  <p className="name">{props.author.name}</p> 
+					  <div className="name">
+					  	<p>
+					  		{props.author.name} 
+					  		<img src={verified} alt="verified icon" />
+					  	</p>
+					  </div> 
 					  <p className="nickname">{props.author.nickname}</p> 
 					  <p className="date">{props.date}</p>
 					</div>
@@ -19,6 +29,12 @@ const Post = (props) =>{
 					<div className="post_image">
 					  <img src={props.image} alt="content"/>
 					</div>
+				</div>
+				<div className="allIcons">
+				  <div className="comentIcon"><img src={comment} alt="comment" /><p>463</p></div>
+				  <div className="likeIcon"><img src={like} alt="like" /><p>784</p></div>
+				  <div className="repostIcon"><img src={repost} alt="repost" /><p>247</p></div>
+				  <div className="shareIcon"><img src={share} alt="comment" /><p></p></div>
 				</div>
 			</div>
 		</div>
